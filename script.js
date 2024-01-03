adc0b3265f4f7056533bca92f5eaa3770633f725
Márcio Hernandes Ribeiro Junior <ribeiro.hernandes97@gmail.com>
2024-01-03 16:12:04 -0300

    Functions em JS

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 let $txt_n = document.getElementById('txt_n')
+let $list = []
 
 function Adicionar() {
 
-    if (!$txt_n.value.length || isNaN($number)) {
+    if (!$txt_n.value.length) {
         console.log('Preencha o campo corretamente')
     } else {
         let $number = Number($txt_n.value)
-
         if ($number < 0 || $number > 100) {
             console.log('Informe um número dentro do esperado')
         }
-
-        else {
-            
+        else if ($list.indexOf($number) != -1) {
+            $list.push($number)
+            let $quad = document.getElementById('quad_number')
+            let $element_option = document.createElement('option')
+            $element_option.textContent = `Valor ${$number} Adicionado`
+            $quad.appendChild($element_option)
+        } else{
+            console.log('Numero já informado')
         }
     }
 }
