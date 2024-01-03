9fa8973b72f1f69da3347714c6703d0b2bbf4de9
Márcio Hernandes Ribeiro Junior <ribeiro.hernandes97@gmail.com>
2024-01-03 17:22:04 -0300

    functio att

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let $txt_n = document.getElementById('txt_n')
+let $msg = document.getElementById('msg')
 let $list = []
 
 function Adicionar() {
@@ -10,14 +11,28 @@ function Adicionar() {
         if ($number < 0 || $number > 100) {
             console.log('Informe um número dentro do esperado')
         }
-        else if ($list.indexOf($number) != -1) {
+        else if ($list.indexOf($number) === -1) {
             $list.push($number)
             let $quad = document.getElementById('quad_number')
             let $element_option = document.createElement('option')
             $element_option.textContent = `Valor ${$number} Adicionado`
             $quad.appendChild($element_option)
-        } else{
+        }
+        else {
             console.log('Numero já informado')
         }
     }
+}
+
+
+function Calcular() {
+    $list.sort(function (a, b) {
+        return a - b
+    })
+
+    $msg.innerHTML = `
+    O maior número da lista é ${$list[(($list.length)-1)]}
+    `
+
+
 }
